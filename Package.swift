3f1079757617ccfa0eb4a4fe4ac91b55f81ab0fb
Daniel Leping <dileping@gmail.com>
2016-02-22 14:52:17 +0200

    Execution Context version

--- Package.swift
+++ Package.swift
@@ -32,6 +32,6 @@ let package = Package(
     ],
     dependencies: [
         .Package(url:"https://github.com/crossroadlabs/Result.git", majorVersion: 1),
-        .Package(url:"https://github.com/crossroadlabs/ExecutionContext.git", majorVersion: 0, minor: 1),
+        .Package(url:"https://github.com/crossroadlabs/ExecutionContext.git", majorVersion: 0),
     ]
 )
